29e189df5728fdb8a2c13eaa271a5775dc633e49
arunchandel86 <119264074+arunchandel86@users.noreply.github.com>
2023-03-24 11:55:14 +0530

    Update sendEmail.php

--- sendEmail.php
+++ sendEmail.php
@@ -33,7 +33,7 @@ $to = $email;
 
 
 $headers = "From:  $email_from \r\n";
-$headers .= "Reply-To: $email \r\n";
+//$headers .= "Reply-To: $email \r\n";
 
 mail($to, $email_subject, $email_body, $headers);
 
