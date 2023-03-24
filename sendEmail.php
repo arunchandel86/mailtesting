7768b6e4c1bcd41ab12f44c9ba43f0fcee261d7f
arunchandel86 <119264074+arunchandel86@users.noreply.github.com>
2023-03-24 15:34:55 +0530

    Update sendEmail.php

--- sendEmail.php
+++ sendEmail.php
@@ -9,41 +9,26 @@ $course = $_POST['course'];
 $date = $_POST['date-details-dropdown'];
 $time = $_POST['time-details-dropdown'];
 
-require "vendor/autoload.php";
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-
-$mail = new PHPMailer(true);
-
-
-$mail->Host = "smtp.gmail.com";
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 465;
-
-$mail->Username = "arun.chandel86@gmail.com";
-$mail->Password = "A1r2un51@";
 
 $email_from = 'info@dnapesci.com';
-$email_subject = 'DNA PESCI - Expression of Interest';
+$email_subject = 'DNA PESCI - Expression of Interest'
 
-$mail->setFrom($email_from , $name);
-$mail->addAddress("arun.chandel86@gmail.com", "Dave");
+$email_body = "Thank you $name for your interest in DNA PESCI Course.\n
+              You have selected $course course starting on $date at $time. \n 
+              Please pay enrolment amount into the following account.  \n 
+              Account Name: DNA PESCI  \n 
+              BSB:   \n 
+              Account No:   \n 
+              Please note that your enrolment confirmation will be sent to you in your email once the course fee is processed. \n 
+              we will contact you on your skype id [$skype] to add you into the group. \n
+              Thanks again for choosing DNA PESCI $course course. \n
+              We wish you the very best for your interview and hope to join you soon. \n\n
+              Warm regards, \n
+              Admin \n
+              DNA PESCI \n"
+              ;  
 
-$mail->Subject = $email_subject;
-
-$email_body = "Thank you $name for your interest in DNA PESCI Course.\n"  ;  
-
-
-$mail->Body = $email_body;
-
-$mail->send();
-
-header("Location: sent.html");
-
-
-
-/*
 $to = $email;
 
 
@@ -58,5 +43,5 @@ if(mail($to, $subject, $email_body, $headers)){
     echo 'Unable to send email. Please try again.';
 }
 */
-//header("Location: index.html"); 
+header("Location: index.html");
 ?>
