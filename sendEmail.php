0f4a24a4d358665bc2a9ff274fc7665a93c09f83
arunchandel86 <119264074+arunchandel86@users.noreply.github.com>
2023-03-24 09:35:02 +0530

    Update sendEmail.php

--- sendEmail.php
+++ sendEmail.php
@@ -1,11 +1,5 @@
 <?php
-$phpmailer = new PHPMailer();
-$phpmailer->isSMTP();
-$phpmailer->Host = 'sandbox.smtp.mailtrap.io';
-$phpmailer->SMTPAuth = true;
-$phpmailer->Port = 2525;
-$phpmailer->Username = '76803de511465e';
-$phpmailer->Password = '9ee82b85cb7911';
+
 
 $name = $_POST['fullname'];
 $email = $_POST['email'];
